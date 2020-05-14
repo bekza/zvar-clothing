49051052f37b1b202eb744f344a8508cff3f5883
Bekzat Omrzakov <bomrzak@gap.com>
2020-05-14 01:58:42 -0700

    Updates

---
+++ precache-manifest.c23ce53cb02a55a0dc5dbd554c35042b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cc68da946f6c8e149a9dbb3692e3a9",
+    "url": "/zvar-clothing/index.html"
+  },
+  {
+    "revision": "fecbd6e9d59c9cadf42a",
+    "url": "/zvar-clothing/static/css/main.cf604f3b.chunk.css"
+  },
+  {
+    "revision": "27446104a0ac0829c200",
+    "url": "/zvar-clothing/static/js/2.b41f502a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zvar-clothing/static/js/2.b41f502a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecbd6e9d59c9cadf42a",
+    "url": "/zvar-clothing/static/js/main.6e130f2f.chunk.js"
+  },
+  {
+    "revision": "72b3b4bdb2ab559863ac",
+    "url": "/zvar-clothing/static/js/runtime-main.561e9d42.js"
+  }
+]);
